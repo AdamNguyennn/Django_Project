00c37181558b53dfc04d5d6d6491eb02c2a47a5b
Your Name <your@email-address.com>
2021-07-24 18:46:43 +0700

    Create Model and using SQLite admin page

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
-class List:
-	id: int
-	name: str
-	description: str
-	is_true: bool
+class List(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=100)
+	
--- project/myapp/views.py
+++ project/myapp/views.py
@@ -6,32 +6,7 @@ from os import name
 # Create your views here.
 def index(request):
     #create list from models and after that push it to templates index.html
-    list1 = List()
-    list1.id = 1
-    list1.name = "study"
-    list1.description = "learning docker, django, devNet"
-    list1.is_true = True
-
-    list2 = List()
-    list2.id = 2
-    list2.name = "cleaning"
-    list2.description = "living room, kitchen, my room "
-    list2.is_true = True
-
-    list3 = List()
-    list3.id = 3
-    list3.name = "do exercise"
-    list3.description = "push up, pull up, abs,..."
-    list3.is_true = False
-    
-    list4 = List()
-    list4.id = 4
-    list4.name = "playing"
-    list4.description = "Lol, cs:go, ...."
-    list4.is_true = True
-    
-    lists = [list1, list2, list3, list4]
-
+    lists = List.objects.all()
     return render(request, "index.html", {'lists': lists})
 
 def count(request):
