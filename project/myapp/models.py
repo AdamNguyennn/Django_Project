a2a9eccbcf2d2ce71ef9027553d88775c778b530
Your Name <your@email-address.com>
2021-07-24 16:48:41 +0700

    use simple models.py at myapp

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class List:
+	id: int
+	name: str
+	description: str
+	is_true: bool
--- project/myapp/views.py
+++ project/myapp/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import List
 from os import name
 
 # Create your views here.
 def index(request):
-	name = "Nam Anh"
-	return render(request, "index.html", {'name': name})
+    #create list from models and after that push it to templates index.html
+    list1 = List()
+    list1.id = 1
+    list1.name = "study"
+    list1.description = "learning docker, django, devNet"
+    list1.is_true = True
+
+    list2 = List()
+    list2.id = 2
+    list2.name = "cleaning"
+    list2.description = "living room, kitchen, my room "
+    list2.is_true = True
+
+    list3 = List()
+    list3.id = 3
+    list3.name = "do exercise"
+    list3.description = "push up, pull up, abs,..."
+    list3.is_true = False
+    
+    list4 = List()
+    list4.id = 4
+    list4.name = "playing"
+    list4.description = "Lol, cs:go, ...."
+    list4.is_true = True
+    
+    lists = [list1, list2, list3, list4]
+
+    return render(request, "index.html", {'lists': lists})
 
 def count(request):
     word = request.POST['text']
